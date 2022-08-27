dca5fdc7aefe8b3da758c4b04ac5fabd674286fd
Parth Khatri <pkhatri@straxisindia.com>
2022-08-27 20:05:28 +0530

    Actions

---
+++ src/Services/Actions/actions.js
@@ -0,0 +1,20 @@
+import {ADD_TO_CART} from '../Constants';
+
+export const addtocart = (data) =>{
+
+    return {
+        type:ADD_TO_CART,
+        data:data
+    }
+
+}
+
+
+// export const removetocart = (data) =>{
+
+//     return {
+//         type:'REMOVE_TO_CART',
+//         data:data
+//     }
+
+// }
